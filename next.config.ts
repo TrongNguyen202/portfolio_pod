bf68dd006f4812f1f0211ccd5eebe26e4e3a92b1
NguyenAnh <anht9402@gmail.com>
2025-07-10 23:34:57 +0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  server: {
+    port: 3001,
+    host: "0.0.0.0",
+  },
 };
 
 export default nextConfig;
